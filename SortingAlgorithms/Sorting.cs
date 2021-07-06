71808c04588175e68d9f2243b508a731249ca598
Mariia Yelisieieva <maria97.55ua@gmail.com>
2021-07-06 22:26:10 +0300

    Add project files.

---
+++ SortingAlgorithms/Sorting.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortingAlgorithms
+{
+    public class Sorting
+    {
+        // O(N2)
+        // stable
+        public static void BubbleSort(int[] array)
+        {
+            for (int wall = array.Length - 1; wall > 0; wall--)
+            {
+                for (int i = 0; i < wall; i++)
+                {
+                    // if (array[i] >= array[i + 1]) => UNSTABLE since duplicate items order in unsaved
+                    if (array[i] > array[i + 1])
+                        Swap(array, i, i + 1);
+                }
+            }
+        }
+
+        private static void Swap(int[] array, int i, int j)
+        {
+            if (i == j)
+                return;
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
---
+++ SortingAlgorithmsTest/SortingTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using SortingAlgorithms;
+using System;
+
+namespace SotringAlgorithmsTest
+{
+    [TestFixture]
+    public class SortingTests
+    {
+        private int[][] Samples()
+        {
+            return new int[9][]
+            {
+                new[] { 1 },
+                new[] { 2, 1 },
+                new[] { 2, 1, 3 },
+                new[] { 2, -1, 3, 3 },
+                new[] { 4, -5, 3, 3 },
+                new[] { 0, -5, 3, 4 },
+                new[] { 0, -5, 6, 0 },
+                new[] { 1, 1, 5, -6 },
+                new[] { 1, 6, 4, 2, 8, 4, 7, 3, 5 },
+            };
+        }
+
+        private void RunSortTest(Action<int[]> sort)
+        {
+            foreach (var sample in Samples())
+            {
+                sort(sample);
+                CollectionAssert.IsOrdered(sample);
+                PrintOutResult(sample);
+            }
+        }
+
+        private void PrintOutResult(int[] sample)
+        {
+            TestContext.WriteLine("--------TRACE-----------");
+            foreach (var el in sample)
+            {
+                TestContext.Write(el + " ");
+            }
+            TestContext.WriteLine("\n--------------------------\n");
+        }
+
+        [Test]
+        public void BubbleSort_ValidInput_SortedInput()
+        {
+            RunSortTest(Sorting.BubbleSort);
+        }
+    }
+}
