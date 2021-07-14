1de6c19cfc30296263da7688e974c20b3b8dfe3a
Mariia Yelisieieva <maria97.55ua@gmail.com>
2021-07-14 22:21:02 +0300

    binary search tree added

---
+++ DataStructures/BinarySearchTree.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataStructures
+{
+    public class BinarySearchTree<T> where T : IComparable<T>
+    {
+        private TreeNode<T> root;
+
+        public TreeNode<T> Get(T value)
+        {
+            return root?.Get(value);
+        }
+
+        public T Min()
+        {
+            if (root == null)
+                throw new InvalidOperationException();
+            return root.Min();
+        }
+
+        public T Max()
+        {
+            if (root == null)
+                throw new InvalidOperationException();
+            return root.Max();
+        }
+
+        public void Insert(T value)
+        {
+            if (root == null)
+                root = new TreeNode<T>(value);
+            else
+                root.Insert(value);
+        }
+
+        public IEnumerable<T> TraverseInOrder()
+        {
+            if (root != null)
+                return root.TraverseInOrder();
+
+            return Enumerable.Empty<T>();
+        }
+    }
+}
